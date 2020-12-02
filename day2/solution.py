95908579cc1f9db2670a3b7dc26c9e2e6c949103
An Hoang <anhoang31415@gmail.com>
2020-12-02 09:46:16 +0000

    day 2

---
+++ day2/solution.py
@@ -0,0 +1,36 @@
+import re
+from typing import Tuple
+
+def parse_line(line: str) -> Tuple[int]:
+    p = re.compile("(\d+)-(\d+) (\w): (\w+)")
+    r = re.match(p, line)
+    return r.groups()
+
+def word_has_number_of_chars(min_n: int, max_n: int, char: str, word: str) -> bool:
+    occurrences = len(re.findall(char, word))
+    return (min_n <= occurrences) and (occurrences <= max_n)
+
+def word_has_one_char_at_index(index1: int, index2: int, char: str, word: str) -> bool:
+    has_char_at_idx1 = (len(word) >= (index1)) and (word[index1-1] == char)
+    has_char_at_idx2 = (len(word) >= (index2)) and (word[index2-1] == char)
+    return has_char_at_idx1 != has_char_at_idx2
+
+def main() -> None:
+    count_1: int = 0
+    count_2: int = 0
+
+    for line in open("input.txt", "r"):
+        nb_1, nb_2, char, password = parse_line(line)
+        nb_1 = int(nb_1)
+        nb_2 = int(nb_2)
+
+        if word_has_number_of_chars(nb_1, nb_2, char, password):
+            count_1 += 1
+
+        if word_has_one_char_at_index(nb_1, nb_2, char, password):
+            count_2 += 1
+
+    print(count_1, count_2)
+
+if __name__ == "__main__":
+    main()
